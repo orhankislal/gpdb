af05f35044f7327bb6f79b933abd52442adb5ae1
Orhan Kislal <okislal@pivotal.io>
2017-06-01 16:52:23 -0700

    clean up orca pipeline

--- concourse/scripts/postgis_check_gpdb2.bash
+++ concourse/scripts/postgis_check_gpdb2.bash
@@ -4,6 +4,7 @@ set -eox pipefail
 
 CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${CWDIR}/common.bash"
+source "${CWDIR}/postgis_check_gpdb.bash"
 
 function prep_compile_gpdb(){
 
@@ -61,7 +62,6 @@ function prep_compile_gpdb(){
         cd /tmp/
         sudo rm master.zip
 
-		# source /opt/gcc_env.sh
 		cd \${base_path}/gpdb_src
 		./configure --with-libxml --with-libxslt --with-python --with-perl --prefix=/tmp/gpdb-deploy --enable-orca
 		make
@@ -75,91 +75,13 @@ function prep_compile_gpdb(){
 }
 
 
-function setup_gpadmin_user() {
-	./gpdb_src/concourse/scripts/setup_gpadmin_user.bash "$TEST_OS"
-}
-
-function prep_setup_postgis() {
-	cat > /opt/setup_postgis.sh <<-EOF
-		set -exo pipefail
-		base_path=\${1}
-		source /tmp/gpdb-deploy/greenplum_path.sh
-		source /opt/gcc_env.sh
-		source \${base_path}/gpdb_src/gpAux/gpdemo/gpdemo-env.sh
-		# export MASTER_DATA_DIRECTORY=\${base_path}/gpdb_src/gpAux/gpdemo/datadirs/qddir/demoDataDir-1
-
-		# cd /tmp/
-		# wget ftp://195.220.108.108/linux/centos/7.3.1611/os/x86_64/Packages/json-c-devel-0.11-4.el7_0.x86_64.rpm
-		# sudo yum install json-c-devel-0.11-4.el7_0.x86_64.rpm
-		sudo yum install -y libxml2-devel
-		sudo yum install -y geos-devel
-		sudo yum install -y proj-devel
-		sudo yum install -y gdal-devel
-		sudo yum install -y expat-devel
-		sudo yum install -y patch
-		sudo yum install -y CUnit CUnit-devel
-		wget ftp://invisible-island.net/byacc/byacc-20170430.tgz
-		tar -xzf byacc-20170430.tgz
-		cd byacc-20170430/
-		./configure --enable-btyacc
-		make
-		sudo make install
-		# export LD_LIBRARY_PATH=/usr/lib64:\$LD_LIBRARY_PATH
-
-		cd \${base_path}/postgis_src/postgis/
-		make remove
-		make prepare
-		cd build/postgis-2.1.5/
-		./configure --with-pgconfig=\$GPHOME/bin/pg_config --with-raster --without-topology --prefix=\$GPHOME --libdir=/usr/lib64
-		make
-		make install
-
-		mv \${base_path}/postgis_src/postgis/build/postgis-2.1.5/regress/loader \${base_path}/postgis_src/postgis/build/postgis-2.1.5/regress/bkp_loader
-
-		chown -R gpadmin:gpadmin \${base_path}/postgis_src
-	EOF
-
-	cat > /opt/install_postgis.sh <<-EOF
-		set -exo pipefail
-		base_path=\${1}
-		source /tmp/gpdb-deploy/greenplum_path.sh
-		source /opt/gcc_env.sh
-		source \${base_path}/gpdb_src/gpAux/gpdemo/gpdemo-env.sh
-		cd \${base_path}/postgis_src/postgis/build/postgis-2.1.5/
-
-		make check
-
-		gpstate -a
-
-		cd \$GPHOME/share/postgresql/contrib/postgis-2.1
-		psql template1 -f postgis.sql
-		psql template1 -f postgis_comments.sql
-		psql template1 -f spatial_ref_sys.sql
-		psql template1 -f rtpostgis.sql
-		psql template1 -f raster_comments.sql
-
-	EOF
-
-	chmod a+x /opt/setup_postgis.sh
-	chmod a+x /opt/install_postgis.sh
-}
-
-
-transfer_ownership_for_postgis() {
-  chown -R gpadmin:gpadmin postgis_src
-  [ -d /tmp/gpdb-deploy ] && chown -R gpadmin:gpadmin /tmp/gpdb-deploy
-}
-
 function make_cluster() {
   set -exo pipefail
   source /tmp/gpdb-deploy/greenplum_path.sh
-  # source /opt/gcc_env.sh
   export BLDWRAP_POSTGRES_CONF_ADDONS=${BLDWRAP_POSTGRES_CONF_ADDONS}
   export LDFLAGS='-L/usr/local/lib/'
   export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
   ldconfig
-  # ldconfig -p | grep libgpopt
-  # echo $LD_LIBRARY_PATH
   # Currently, the max_concurrency tests in src/test/isolation2
   # require max_connections of at least 129.
   export DEFAULT_QD_MAX_CONNECT=150
